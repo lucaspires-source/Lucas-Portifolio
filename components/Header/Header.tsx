8aaf3518371d95255ed3727fdbc5432f3853f1a4
lucaspires-source <pires.lucas94@gmail.com>
2023-06-18 09:15:38 -0300

    started working with animation and code cleaning

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,12 +1,22 @@
-import React from "react";
 import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion";
 
-type Props = {};
-
-export default function Header({}: Props) {
+export default function Header() {
   return (
     <header className=" sticky top-0 flex items-start justify-between p-5 max-w-7xl mx-auto z-20 xl:items-center">
-      <div className="flex flex-rows items-center">
+      <motion.div
+        initial={{
+          x: -500,
+          opacity: 0,
+          scale: 0.5,
+        }}
+        animate={{
+            x:0,
+            opacity:1,
+            scale:1
+        }}
+        className="flex flex-rows items-center"
+      >
         <SocialIcon
           url="https://www.linkedin.com/in/lucas-ribeiro-pires/"
           fgColor="gray"
@@ -17,7 +27,7 @@ export default function Header({}: Props) {
           fgColor="gray"
           bgColor="transparent"
         />
-      </div>
+      </motion.div>
       <div className="flex flex-row items-center text-gray-300 cursor-pointer p-1">
         <SocialIcon
           className="cursor-pointer"
